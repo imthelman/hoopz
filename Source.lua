a59d79892f4504908b17091c5a43a1854f75e2ec
imthelman <117380301+imthelman@users.noreply.github.com>
2023-02-22 15:16:22 -0500

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,5 @@
+if syn then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/imthelman/LMan-s-Hoopz-UI/main/synapse.lua", true))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/imthelman/LMan-s-Hoopz-UI/main/non-synapse.lua", true))()
+end
