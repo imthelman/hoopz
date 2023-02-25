e998af68911dbdbb6b40cc60be4227a26cff6809
imthelman <117380301+imthelman@users.noreply.github.com>
2023-02-25 07:50:15 -0500

    Update and rename Source.lua to Source

--- Source.lua
+++
@@ -1,5 +0,0 @@
-if syn then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/imthelman/LMan-s-Hoopz-UI/main/synapse.lua", true))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/imthelman/LMan-s-Hoopz-UI/main/non-synapse.lua", true))()
-end
